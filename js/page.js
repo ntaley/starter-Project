f19d902c3b9bbd80d6ae7017861fb7926ff16087
Ntale Shafik <shafiquentale@gmail.com>
2023-02-06 17:08:41 +0200

    Second commit

--- js/page.js
+++ js/page.js
@@ -5,29 +5,36 @@ let ctx2 = document.getElementById("deptSales").getContext("2d");
 let yearlyLabel = document.getElementById("yearlyTotal");
 
 
-let monthlySales = Array.of(120, 1300, 150);
+let monthlySales = Array.of(500, 9000, 3000);
 let monthlyLabels = Array.of("Jan", "Feb", "Mar");
 
 let deptSales = Array.of(120, 13, 150);
 let deptLables = Array.of("Hiking", "Running", "Hunting");
 
-let janNums = Array.of(120, 13000, 2200);
-let febNums = Array.of(120, 13000, 2200);
-let marNums = Array.of(120, 13000, 2200);
+let yearlyTotal = 0;
 
-function addYearlyIncome(q,w,e) {
-   return q + w + e
+function addYearlyTotal(x) {
+   yearlyTotal = x + yearlyTotal
 }
 
-let yearlyIncome = Array.of(addYearlyIncome(...janNums),addYearlyIncome( ...febNums),addYearlyIncome( ...marNums));
-let total = addYearlyIncome(...yearlyIncome)
-yearlyLabel.innerHTML = "$" + total;
+monthlySales.forEach(addYearlyTotal);
 
-function find1000() {
-    let findThousand = monthlySales.find(element => element > 1000);
-    alert(findThousand);
+let janNums = Array.of(1300, 1000, 4200);
+let febNums = Array.of(1100, 2000, 9000);
+let marNums = Array.of(4000, 1000, 5000);
+//let yearlyIncome = Array.of(addYearlyTotal(...janNums),addYearlyTotal( ...febNums),addYearlyTotal( ...marNums));
+
+yearlyLabel.innerHTML = "$" + yearlyTotal;
+
+function findOver1000() {
+    let firstThousand = monthlySales.findIndex(element => element > 1000);
+    alert(firstThousand);
 }
+function resetNum() {
+    monthlySales.fill(0);
+    monthlySalesChart.update();
 
+}
 
 //Bar
 
@@ -39,9 +46,9 @@ let monthlySalesChart = new Chart(ctx, {
             label: "# of sales",
             data:monthlySales ,
             backgroundColor: [
-                "rgba(255, 0, 100, 1)",
-                "rgba(239, 100, 0, 1)",
-                "rgba(239, 50, 50, 1)",
+                "rgba(256, 167, 100, 1)",
+                "rgba(75, 166, 223, 1)",
+                "rgba(239, 118, 122, 1)",
 
 
             ],
@@ -58,9 +65,9 @@ let deptSalesChart = new Chart(ctx2, {
             label: "# of sales",
             data: deptSales,
             backgroundColor: [
+                "rgba(238, 184, 104, 1)",
+                "rgba(75, 166, 223, 1)",
                 "rgba(239, 118, 122, 1)",
-                "rgba(239, 118, 188, 1)",
-                "rgba(239, 118, 0, 1)",
 
 
             ],
